a80c198bf36168ab919bf9c7e49271cccfb2665d
jkny <jknyproductions@gmail.com>
2018-07-23 02:33:02 -0400

    upload files

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-product',
-    templateUrl: './products.component.html'
+    templateUrl: './products.component.html',
+    styleUrls: ['./products.component.css']
 })
 
 export class ProductComponent {
---
+++ src/app/products/products.model.ts
@@ -0,0 +1,8 @@
+export interface IProduct {
+        _id:string;
+        name:string;
+        language:string;
+        rate:number;
+        type:string;
+        imageUrl:string;
+}
